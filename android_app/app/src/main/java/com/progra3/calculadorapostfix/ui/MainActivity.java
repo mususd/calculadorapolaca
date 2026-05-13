3b39f0ec2e373e3aa40d461287ebc38604f3bdea
Diego <dmmusus@gmail.com>
2026-05-13 17:05:15 -0600

    Fase 1, 2 y 3 completadas

--- android_app/app/src/main/java/com/progra3/calculadorapostfix/ui/MainActivity.java
+++ android_app/app/src/main/java/com/progra3/calculadorapostfix/ui/MainActivity.java
@@ -9,6 +9,8 @@ import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
 
 import com.progra3.calculadorapostfix.R;
+import android.util.Log;
+import com.progra3.calculadorapostfix.utils.Pila;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -22,5 +24,20 @@ public class MainActivity extends AppCompatActivity {
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
             return insets;
         });
+        Pila<Integer> pila = new Pila<>();
+
+        pila.push(10);
+
+        pila.push(20);
+
+        pila.push(30);
+
+        Log.d("PILA", "Peek: " + pila.peek());
+
+        Log.d("PILA", "Pop: " + pila.pop());
+
+        Log.d("PILA", "Peek actual: " + pila.peek());
+
+        Log.d("PILA", "Size: " + pila.size());
     }
 }

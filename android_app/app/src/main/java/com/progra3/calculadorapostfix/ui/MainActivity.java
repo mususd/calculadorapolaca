90970d120a06ccc96cd4cca50b1bf1a8911e2fbd
Diego <dmmusus@gmail.com>
2026-05-13 17:15:15 -0600

    Ejemplo en fase 3

--- android_app/app/src/main/java/com/progra3/calculadorapostfix/ui/MainActivity.java
+++ android_app/app/src/main/java/com/progra3/calculadorapostfix/ui/MainActivity.java
@@ -24,6 +24,7 @@ public class MainActivity extends AppCompatActivity {
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
             return insets;
         });
+        //ejemplo
         Pila<Integer> pila = new Pila<>();
 
         pila.push(10);
